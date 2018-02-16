89aebff1c0aa3d981c7b31b663bbc2658d28a1db
childressdev <jcampbell@childressagency.com>
2018-02-16 17:15:38 -0500

    added parallax for learn more section, other tweaks

--- page-contact.php
+++ page-contact.php
@@ -3,13 +3,11 @@
     <div class="container-fluid container-sm-height">
       <div class="row row-sm-height">
         <div class="col-sm-6 col-sm-height">
-          <div class="contact-form-wrapper">
             <article>
               <?php if(have_posts()): while(have_posts()): the_post(); ?>
                 <?php the_content(); ?>
               <?php endwhile; endif; ?>
             </article>
-          </div>
         </div>
         <div class="col-sm-6 col-sm-height map-side">
           <div class="google-map">
